12e87e7b0ec50497f4e9ff88f4bb12e51d83e300
SaraiReyes <idalia_sarai@hotmail.com>
2021-04-08 20:32:09 -0500

    leerpaises listo

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 
+import requests
+import json
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "cfd5cfefbamshfc97201376bac03p1784f2jsnd1f9137689e5",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response =requests.request("GET", url, headers=headers).json()
+
+pythonDictionary = {'name':'Bob', 'age':44, 'isEmployed':True}
+dictionaryToJson = json.dumps(pythonDictionary)
+
+
+
+
+
 # Create your views here.
 def helloworldview(request):
-    return render(request,'helloworld.html')
+    noofresults=int(response['results'])
+    mylist=[]
+    print(noofresults)
+
+    for x in range(noofresults):        
+        mylist.append(response['response'][x]['country'])
+    
+    context={'mylist': mylist}
+    
+    return render(request,'helloworld.html',context)
